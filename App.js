212e150208b02c621d36d6550e4a16e1b98ca619
danielschwartz3 <71156919+danielschwartz3@users.noreply.github.com>
2020-09-19 18:19:55 -0400

    Navigation linked login and register
    UI still needs work but can navigate between register and login screens

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
@@ -9,19 +9,41 @@ import {color} from 'react-native-reanimated';
 
 const Stack = createStackNavigator();
 
-const App = () => {
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}>
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
-        {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
-        <Stack.Screen name="Registration" component={RegistrationScreen} />
+      <Stack.Navigator>
+        {user ? (
+          <Stack.Screen name="Home">
+            {(props) => <HomeScreen {...props} extraData={user} />}
+          </Stack.Screen>
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Registration" component={RegistrationScreen} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
-export default App;
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator
+//         screenOptions={{
+//           headerShown: false,
+//         }}>
+//         {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+//         {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
+//         <Stack.Screen name="Registration" component={RegistrationScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default App;
