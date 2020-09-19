0b074b0d6de0e153900850ca561a2f6c42df2f68
danielschwartz3 <71156919+danielschwartz3@users.noreply.github.com>
2020-09-19 18:16:01 -0400

    Added login and registration screens

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import RegistrationScreen from './src/screens/RegistrationScreen';
+
 import {color} from 'react-native-reanimated';
 
 const Stack = createStackNavigator();
@@ -13,7 +16,9 @@ const App = () => {
         screenOptions={{
           headerShown: false,
         }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
+        <Stack.Screen name="Registration" component={RegistrationScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
