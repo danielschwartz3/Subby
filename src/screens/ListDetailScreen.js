b9a460ac13d437be25abe2dfb828e396cda0d96d
Sivam Agarwalla <sivam.agarwalla@gmail.com>
2020-09-20 05:58:17 -0700

    Finished mainly. Ready for demo

--- src/screens/ListDetailScreen.js
+++ src/screens/ListDetailScreen.js
@@ -9,7 +9,7 @@ import {
   Paragraph,
   Provider as PaperProvider,
 } from 'react-native-paper';
-import {View} from 'react-native';
+import {Image, View} from 'react-native';
 
 export default function ListDetailScreen({route, navigation}) {
   const {item} = route.params;
@@ -26,7 +26,7 @@ export default function ListDetailScreen({route, navigation}) {
       <Card>
         <Card.Title
           title={`${item.hostName}`}
-          subtitle={`${item.phoneNumber}`}
+          subtitle={`Phone Number: ${item.phoneNumber}`}
           // left={sub_icon}
           style={{padding: 15}}
         />
@@ -35,9 +35,9 @@ export default function ListDetailScreen({route, navigation}) {
           <Paragraph>{`${item.addressLineOne}`}</Paragraph>
           <Paragraph>{`${item.addressLineTwo}`}</Paragraph>
           <Paragraph>{`${item.city}, ${item.state}, ${item.zipCode}`}</Paragraph>
-          <Paragraph>{`${item.startDate} - ${item.endDate}`}</Paragraph>
+          <Paragraph>{`Available from ${item.startDate} to ${item.endDate}`}</Paragraph>
         </Card.Content>
-        <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
+        <Card.Cover source={{uri: item.photoURI}} />
         <Card.Actions>
           <View style={{paddingRight: 5}}>
             <Chip style={{backgroundColor: '#1C2541'}} mode="outlined">
